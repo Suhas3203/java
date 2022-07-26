4a3e5b557cf462a291840ffdd25efc4e6393e508
Suhas3203 <109971031+Suhas3203@users.noreply.github.com>
2022-07-26 17:11:00 +0530

    Add files via upload
    Hey buddy, I've done something about datatypes explained yesterday. Check it out.

---
+++ practiceDatatypes.java
@@ -0,0 +1,20 @@
+package com.javaprogram1;
+
+public class practiceDatatypes {
+
+	
+
+	public static void main(String []args) {
+		boolean isProcessDone= true;
+		
+		char fLetterOfName='S';
+		
+		byte ageOfPerson=27;
+		
+		System.out.println(isProcessDone);
+		
+		System.out.println(fLetterOfName);
+		
+		System.out.println(ageOfPerson);
+	}
+}
